8e2435a999f5b31929b90b82a68629836b94c56a
mors741 <mors741@yandex.ru>
2017-09-17 21:47:10 +0300

    authentication

--- src/main/java/com/staslabs/xstas/config/SecurityConfig.java
+++ src/main/java/com/staslabs/xstas/config/SecurityConfig.java
@@ -6,8 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 
 @Configuration
@@ -15,27 +14,28 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private DataSource dataSource;
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/user/profile/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
-            .formLogin()
+                .formLogin()
                 .loginPage("/login")
-            .and()
-            .httpBasic();
+                .and()
+                .httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email, password, 1 FROM users WHERE email=?")
-                .authoritiesByUsernameQuery("SELECT email, role_name FROM users U INNER JOIN roles R ON U.role = R.role_id WHERE email=?");
+        auth.userDetailsService(userDetailsService);
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
---
+++ src/main/java/com/staslabs/xstas/config/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.staslabs.xstas.config;
+
+import com.staslabs.xstas.data.UserRepository;
+import com.staslabs.xstas.data.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.getByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user present with email: " + email);
+        } else {
+            String role = user.getRole().toString();
+            Collection<GrantedAuthority> rolesCollection = Collections.singleton(new SimpleGrantedAuthority(role));
+            return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), rolesCollection);
+        }
+    }
+}
